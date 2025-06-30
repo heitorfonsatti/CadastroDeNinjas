ac16c5ed51beb8ffbb324440a0dc976dea7536c2
heitorfonsatti <125671020+heitorfonsatti@users.noreply.github.com>
2025-06-30 15:56:47 -0300

    NinjaModel Created

---
+++ src/main/java/dev/heitor/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,58 @@
+package dev.heitor.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+// Entity transforma uma classe em uma entidade do banco de dados
+// JPA = Java Persistence API
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private  int age;
+
+    // No args constructor
+    public NinjaModel() {
+    }
+
+    // Constructor
+    public NinjaModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
